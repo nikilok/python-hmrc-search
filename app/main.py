5fd1bfcf6043ab7ea07b04640f64f9c55b379556
nikilok <nikilok@gmail.com>
2025-07-08 15:22:23 +0100

    re-run formatter

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 mcp = FastApiMCP(app)
 mcp.mount()
 
+
 @app.get("/search", response_model=List[CompanySearchResult])
 def search_company(
     company_name: str = Query(..., description="Company name to search for")
@@ -19,5 +20,6 @@ def search_company(
             status_code=400, detail="company_name must be at least 3 characters long."
         )
     return search_companies(company_name)
+
 
 mcp.setup_server()
